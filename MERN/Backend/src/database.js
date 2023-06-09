02f655fb70837f97fc8792ccf4bcf521941d22c7
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2023-06-09 09:59:46 -0500

    se cambia la cadena de conexion de mongo atlas porque el dns no me permitia conectarme a la cadena anterior

--- MERN/Backend/src/database.js
+++ MERN/Backend/src/database.js
@@ -10,11 +10,15 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const { PORT, MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTNAME } = process.env;
-const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/?retryWrites=true&w=majority`;
+//const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/?retryWrites=true&w=majority`;
+//conexion mongodb nodejs 2.2.12, el DNS mio no permite conexion al dominio srv de las versiones superiores
+const MONGO_URI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@ac-d7zwzlz-shard-00-00.tgmtfzp.mongodb.net:27017,ac-d7zwzlz-shard-00-01.tgmtfzp.mongodb.net:27017,ac-d7zwzlz-shard-00-02.tgmtfzp.mongodb.net:27017/?ssl=true&replicaSet=atlas-vakvuk-shard-0&authSource=admin&retryWrites=true&w=majority`
 
 mongoose
   .connect(MONGO_URI, {
     dbName: "sample_training",
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
   .then((db) => console.log("connected database"))
   .catch((err) => console.log("error connecting", err));
--- MERN/Backend/src/index.js
+++ MERN/Backend/src/index.js
@@ -1,6 +1,5 @@
 import express from "express";
-//TODO: verificar que funcione la conexión a mongo atlas y descomentar la siguiente línea
-//import database from "./database.js"; // ES6
+import database from "./database.js"; // ES6
 //require("./database"); // CommonJS
 import postRouter from "./routes/postRoute.js";
 import morgan from "morgan";
@@ -14,7 +13,6 @@ app.use(morgan('dev')); //registra información sobre las solicitudes HTTP
 
 // Routes
 app.use('/api/', postRouter);
-//app.get("/", (req, res) => res.send("API is running"));
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
